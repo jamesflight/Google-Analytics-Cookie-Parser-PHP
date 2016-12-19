d34d01942fb1d44a01caf758ad42e4d886f50421
Clement Bourgoin <cb@nokto.net>
2016-12-19 13:07:49 +0100

    Add tests cases for parseString method

--- tests/GACookie/ParserTest.php
+++ tests/GACookie/ParserTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use Jflight\GACookie\Parser;
+use Jflight\GACookie\GACookie;
 use Mockery as m;
 
 class ParserTest extends PHPUnit_Framework_TestCase {
@@ -44,7 +45,23 @@ class ParserTest extends PHPUnit_Framework_TestCase {
 		$result = $parser->parse('utmz');
 		$this->assertSame(false,$result);
 	}
-	
+
+	public function testCanParseUtmaString()
+	{
+		$utma = GACookie::parseString('utma', '177910838.254655113.1474876189.1482142331.1482148790.58');
+		$this->assertEquals('2016-09-26 07:49:49', $utma->time_of_first_visit->format('Y-m-d H:i:s'));
+		$this->assertEquals('2016-12-19 10:12:11', $utma->time_of_last_visit->format('Y-m-d H:i:s'));
+		$this->assertEquals('2016-12-19 11:59:50', $utma->time_of_current_visit->format('Y-m-d H:i:s'));
+		$this->assertEquals(58, $utma->session_count);
+	}
+
+	public function testCanParseUtmzString()
+	{
+		$utmz = GACookie::parseString('utmz', '177910838.1481550491.52.15.utmcsr=newsletter|utmccn=campaign-2016|utmcmd=email');
+		$this->assertEquals('newsletter', $utmz->source);
+		$this->assertEquals('email', $utmz->medium);
+		$this->assertEquals('campaign-2016', $utmz->campaign);
+	}
 
 	protected function getParser()
 	{
